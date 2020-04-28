0b85efffef1ba82d120a948e1a1c4a341dd88e6e
alicj <alicjakowalska0624@gmail.com>
2020-04-29 00:41:00 +0200

    4thcommit

--- app/src/main/java/com/example/padtm_lab04/MainActivity.java
+++ app/src/main/java/com/example/padtm_lab04/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.padtm_lab04;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -53,6 +54,18 @@ public class MainActivity extends AppCompatActivity {
         Intent intencja = new Intent(this,
                 DodajWpis.class);
         startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
     }
 
 }
